56129ad6527d1a9cca753ea01884b5e0ee19f390
javier <javierperez.od@gmail.com>
2021-04-26 13:03:46 -0300

    Primer commit

---
+++ helpers/multiplicar.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+require('colors');
+
+
+const crearArchivo = async( base = 5, listar = false, hasta = 10 ) => {
+
+    try {
+        
+        let salida = ''; 
+
+        for( let i = 1; i <= hasta; i++ ) {
+            salida+= (`${ base } x ${ i } = ${ base * i }\n`);
+        }
+
+        if ( listar ) {
+            console.log('===================='.green);
+            console.log('    Tabla del:'.green,colors.blue( base ) );
+            console.log('===================='.green);
+            
+            console.log(salida);
+        }
+
+        
+
+        fs.writeFileSync(`./salida/tabla-${ base }.txt`, salida );
+
+        return `tabla-${ base }.txt`;
+                
+        } catch (err) {
+            throw err;
+                
+        }
+
+    
+
+}
+
+module.exports = {
+    crearArchivo
+
+}
